34d9ed5f0be9efd053d864142b74ae7f53f56038
dpinilla <ingdiegopinilla@hotmail.com>
2026-09-10 07:41:51 -0500

    actualización

--- app/src/main/java/com/dap/crudcomposepestanas/conexionService/ConexionService.kt
+++ app/src/main/java/com/dap/crudcomposepestanas/conexionService/ConexionService.kt
@@ -2,6 +2,6 @@ package com.dap.crudcomposepestanas.conexionService
 
 class ConexionService {
     companion object{
-        val url: String = "http://192.168.0.171/adso09crud/"
+        val url: String = "http://192.168.1.7/adso09crud/"
     }
 }
